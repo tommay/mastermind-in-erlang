ef58dc86d88249f8c218dc2d97359e664c3253b7
Tom May <tom@tommay.net>
2015-03-16 13:56:02 -0700

    Do the max in parallel.

--- mastermind.erl
+++ mastermind.erl
@@ -2,6 +2,7 @@
 -export([new/0, new/1, new/3, random_code/0, size/1, make_guess/1,
 	 compute_score/2]).
 -compile({no_auto_import, [size/1]}).
+-compile(export_all).
 
 -record(mastermind, {use_all_codes = false, codes, all_codes, all_scores}).
 
@@ -80,9 +81,23 @@ best_guess(This) ->
     parallel_min_by(
       Codes,
       fun (Guess) ->
-	      lists:max(
-		[size(new(This, Guess, Score)) ||
-		    Score <- This#mastermind.all_scores])
+	      worst_case(This, Guess)
+      end).
+
+worst_case(This, Guess) ->
+    lists:max(
+      [size(new(This, Guess, Score)) || Score <- This#mastermind.all_scores]).
+
+%% The parallel_max version works fine, but erl seems to choke when
+%% the number of processes gets too high.  1296 codes times 14 scores
+%% == 18144 processes, althouth some of them should be finished by the
+%% time they're all started.
+%%
+parallel_worst_case(This, Guess) ->
+    parallel_max(
+      This#mastermind.all_scores,
+      fun (Score) ->
+	      size(new(This, Guess, Score))
       end).
 
 compute_score(Code, Guess) ->
@@ -108,23 +123,50 @@ parallel_min_by(List, Func) ->
     Ref = make_ref(),
     lists:foreach(
       fun (Element) ->
-	  spawn(fun () -> Self ! {Ref, Func(Element), Element} end)
+	      spawn(fun () -> Self ! {Ref, Func(Element), Element} end)
       end,
       List),
-    collect_min(Ref, length(List)).
+    collect_min_by(Ref, length(List)).
 
-collect_min(Ref, N) ->
-    collect_min(Ref, N, undefined, undefined).
+collect_min_by(Ref, N) ->
+    collect_min_by(Ref, N, undefined, undefined).
 
-collect_min(_Ref, 0, _Min, Element) ->
+collect_min_by(_Ref, 0, _Min, Element) ->
     Element;
-collect_min(Ref, N, Min, Element) ->
+collect_min_by(Ref, N, Min, Element) ->
     receive
 	{Ref, Value, NewElement} ->
 	    case (Min == undefined) orelse (Value < Min) of
 		true ->
-		    collect_min(Ref, N - 1, Value, NewElement);
+		    collect_min_by(Ref, N - 1, Value, NewElement);
 		false ->
-		    collect_min(Ref, N - 1, Min, Element)
+		    collect_min_by(Ref, N - 1, Min, Element)
 	    end
     end.
+
+parallel_max(List, Func) ->
+    Self = self(),
+    Ref = make_ref(),
+    lists:foreach(
+      fun (Element) ->
+	      spawn(fun () -> Self ! {Ref, Func(Element)} end)
+      end,
+      List),
+    collect_max(Ref, length(List)).
+
+collect_max(Ref, N) ->
+    collect_max(Ref, N, undefined).
+
+collect_max(_Ref, 0, Max) ->
+    Max;
+collect_max(Ref, N, Max) ->
+    receive
+	{Ref, Value} ->
+	    case (Max == undefined) orelse (Value > Max) of
+		true ->
+		    collect_max(Ref, N - 1, Value);
+		false ->
+		    collect_max(Ref, N - 1, Max)
+	    end
+    end.
+
